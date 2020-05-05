27cca791a168f9e71faf3e99d9dc350192262659
Mateus <mateusmoran@gmail.com>
2020-05-05 16:25:29 -0300

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,26 @@
+console.log('Client side javascript file is loaded!')
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const mensageOne = document.querySelector('#mensage-1')
+const mensageTwo = document.querySelector('#mensage-2')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+
+    mensageOne.textContent = 'Loadind...'
+    mensageTwo.textContent = ''
+
+    fetch('http://localhost:3000/weather?address=' + location).then ((response) => {
+    response.json().then((data) => {
+        if (data.error){
+            mensageOne.textContent = data.error
+        } else {
+            mensageOne.textContent = data.location
+            mensageTwo.textContent = data.forecast
+        }
+    })
+})
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,25 @@
+const request = require('request')
+
+const forecast = (lat, long, callback) => {
+
+    const url = 'http://api.weatherstack.com/current?access_key=c39b5c06e0e36cd430b8bd468b1b0b5b&query='+ lat +',' + long +'&units=f'
+
+    request({url, json: true }, (error, { body } = {}) => {
+        if(error){
+            callback('unable to connect to weather service!', undefined)
+        } else if(body.error){
+            callback('Unable to find weather information', undefined)
+        } else {
+            callback(undefined, {
+                descriptions: body.current.weather_descriptions[0],
+                temperature: body.current.temperature,
+                feelslike: body.current.feelslike
+            }                
+            )
+            
+        }
+        
+    })
+}
+
+module.exports = forecast
